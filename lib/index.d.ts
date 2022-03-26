e9dbc2adb2f19d1301b69555105938128c3f4a93
kerimCetinbas <a.kerim.cetinbas@yandex.com>
2022-03-26 16:58:46 +0300

    remove auto genereted

--- lib/index.d.ts
+++
@@ -1,8 +0,0 @@
-import { Microp, MicropRouter } from "./microp";
-export { Microp, MicropRouter };
-declare const _default: {
-    Microp: typeof Microp;
-    MicropRouter: typeof MicropRouter;
-};
-export default _default;
-//# sourceMappingURL=index.d.ts.map
